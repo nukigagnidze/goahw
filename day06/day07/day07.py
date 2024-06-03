3b7651fc33bdd5f92d6c4dae202a59a78f20b6f0
nukigagnidze <marisefashvili32@gmail.com>
2024-06-03 10:00:29 -0700

    2013

---
+++ day06/day07/day07.py
@@ -0,0 +1,7 @@
+<p>hello eorld!</p> 
+<h1>Im nuki gagnidze</h1>
+<h2>Im 10 years old,/h2>
+<h3>I like flovers</h3>
+<h4>I have three brothers</h4>
+<h5>I love goa</h5>
+<h6>my best frend is nia kaxashvili</h6>
