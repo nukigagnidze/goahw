84481e0a9abd8c6a578b352bd82d53d541a30fe6
nukigagnidze <marisefashvili32@gmail.com>
2024-06-06 13:20:37 -0700

    2013

--- day06/day07/day07/day08/day08/day08.py
+++ day06/day07/day07/day08/day08/day08.py
@@ -1,9 +1,33 @@
-num1 = 40 
-num2 = 50 
-num3 = 60
+# num1 = 40 
+# num2 = 50 
+# num3 = 60
 
-print(num1 + num2 + num3)
-print(num1 - num2 - num3)
-print(num1 * num2 * num3)
-print(num1 / num2 / num3)
-# print <num1 + num2 + num3> = აქ შეცდომაა რადგან <> ამეების მაგუვრად უნდა ეწეროს = ეს
+# print(num1 + num2 + num3)
+# print(num1 - num2 - num3)
+# print(num1 * num2 * num3)
+# print(num1 / num2 / num3)
+# print <num1 + num2 + num3> = აქ შეცდომაა რადგან <>ამის მაგივრად ეს უნდა ეწეროს=
+
+
+
+
+
+
+
+
+
+
+snake_case = 600
+print(snake_case)
+snake_case = 1000
+print(snake_case)
+snake_case = 2000
+print(snake_case)
+snake_case = 1000000
+print(snake_case)
+snake_case = 2000000
+print(snake_case)
+
+
+
+# ამ დავალებაში გამოვიყენე  snake_case და print  
