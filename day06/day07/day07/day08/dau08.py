1107bcc11beefa825aff319d27feda7b610fac25
nukigagnidze <marisefashvili32@gmail.com>
2024-06-06 12:39:31 -0700

    2013

---
+++ day06/day07/day07/day08/dau08.py
@@ -0,0 +1,6 @@
+#making a variables
+chad = "students and teachers"
+
+#becoming chad
+level = 8
+goal = 2500
---
+++ day06/day07/day07/day08/day08/day08.py
@@ -0,0 +1,9 @@
+num1 = 40 
+num2 = 50 
+num3 = 60
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+# print <num1 + num2 + num3> = აქ შეცდომაა რადგან <> ამეების მაგუვრად უნდა ეწეროს = ეს
