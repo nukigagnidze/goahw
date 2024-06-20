f6c925790adf583e5dc8ee165661f0ed2e309c9a
nukigagnidze <marisefashvili32@gmail.com>
2024-06-20 10:14:03 -0700

    2013

---
+++ day09/day010/day011/day011/day012/day012.py
@@ -0,0 +1,60 @@
+print("5" + "5")
+print("num" + "num")
+print("nuki" + "nuki")
+print("gagnidze" + "gagnidze")
+print("hobby" + "hobby")
+print("roze" + "roze")
+print("cat" + "cat")
+print("dog" + "dog")
+print("ball" + "ball")
+print("ice qriam" + "ice qriam")
+print("iphon" + "iphon")
+print("toy" + "toy")
+print("pen" + "pen")
+print("pencil" + "pencil")
+print("notebook" + "notebook")
+print("tv" + "tv")
+print("egg" + "egg")
+print("car" + "car")
+print("flover" + "flovers")
+print("vase" + "vase")       
+
+
+name = input("enter your name")
+age = input("enter your age")
+surname = input("enter your surname")
+book = input("enter your fevorite book ")
+pet = input("enter your fevorit pet")
+weather = input("enter your fevorit weather")
+flower = input("enter your fevorit flover")
+contri = input("enter your contei")
+food = input("enter your fevorit food")
+mom = input("enter your moms name")
+dad = input("enter your dads name")
+broters = input("enter your broters name")
+grendma = input("enter your grendmas name")
+color = input("enter your fevorit color")
+fruit = input("enter your fevorit fruit")
+hobby = input("enter your hobby")
+broters_age =("enter your broters age")
+lesson = input("enter your fevorit lesson")
+techer = input("enter your fevorit techer")
+clas = input ("enter your class")           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
