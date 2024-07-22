a6409e926de8b7040a087df2c3de7193a1fe21c9
nukigagnidze <marisefashvili32@gmail.com>
2024-07-22 11:19:56 -0700

    2013

---
+++ day31/day31/day31.py
@@ -0,0 +1,29 @@
+13
+print(type(13))
+
+20.5
+print(type(10.5))
+
+"Hello World!"
+print(type("Hello World!")) 
+
+
+age1 = input("please enter your age: ")
+age2 = 10
+if age1 == age2:
+    print(True)
+else:
+    print(False)
+
+
+name1= "gio"
+
+print("In Goa My Leader’s name is {name1}")
+
+name2= "varadashvili"
+
+print("gio‵s Last Name Is {name2}")
+
+age1= 15
+
+print("gio Varadashvili‵s age is {age1}")
